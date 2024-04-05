803b4a30175d104ccd7fa99d5e64e82f4aa2954f
Peter Taoussanis <peter@taoensso.com>
2024-04-05 18:24:11 +0200

    v1.0.0-alpha3 (2024-04-05)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/telemere "1.0.0-alpha2"
+(defproject com.taoensso/telemere "1.0.0-alpha3"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Structured telemetry library for Clojure/Script"
   :url "https://www.taoensso.com/telemere"
