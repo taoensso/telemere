8276f5c1811bb7e4133e40c057b50691e27bac33
Peter Taoussanis <peter@taoensso.com>
2024-08-19 15:12:07 +0200

    v1.0.0-beta17 (2024-08-19)

--- slf4j/project.clj
+++ slf4j/project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/slf4j-telemere "1.0.0-beta16"
+(defproject com.taoensso/slf4j-telemere "1.0.0-beta17"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Telemere backend/provider for SLF4J API v2"
   :url "https://www.taoensso.com/telemere"
@@ -16,7 +16,7 @@
    {:dependencies
     [[org.clojure/clojure   "1.11.4"]
      [org.slf4j/slf4j-api   "2.0.16"]
-     [com.taoensso/telemere "1.0.0-beta16"]]}
+     [com.taoensso/telemere "1.0.0-beta17"]]}
 
    :dev
    {:plugins
