b7d2b4a1ed742405a508dad0766dda3577f7b6e3
Peter Taoussanis <peter@taoensso.com>
2025-02-27 12:37:07 +0100

    [nop] Bump deps

--- slf4j/project.clj
+++ slf4j/project.clj
@@ -17,7 +17,7 @@
   {:provided
    {:dependencies
     [[org.clojure/clojure   "1.12.0"]
-     [org.slf4j/slf4j-api   "2.0.16"]
+     [org.slf4j/slf4j-api   "2.0.17"]
      [com.taoensso/telemere "1.0.0-SNAPSHOT"]]}
 
    :dev
