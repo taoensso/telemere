taoensso/telemere

commit 47af80319dd6a6922ad9617cbebc3dc632fdb689

[mod] [fix] Timbre->Telemere appender: de-duplicate output formatting
BEFORE this commit:

  The Timbre->Telemere appender produced duplicate preamble output
  (timestamp, namespace, etc.). Both Timbre AND Telemere were adding
  preamble info.

AFTER this commit:

  Now ONLY Telemere adds preamble info.
  Timbre's `:output-fn` is ignored.

main/src/taoensso/telemere/timbre.cljc
@@ -190,8 +190,11 @@
    :min-level nil
    :fn
    (fn [data]
-     (let [{:keys [instant level context ?err output_
-                   ?ns-str ?file ?line ?column]} data]
+     (let [{:keys [instant level context msg-type vargs
+                   ?ns-str ?file ?line ?column]} data
+
+           format-msg? (enc/identical-kw? msg-type :f)
+           [error msg vargs] (parse-vargs format-msg? vargs)]
 
        (taoensso.telemere/signal!
          {:kind :timbre
@@ -203,5 +206,6 @@
           :coords (when ?line [?line ?column])
           :file   ?file ; Non-standard, goes to kvs
 
-          :error  ?err
-          :msg    (force output_)})))})
+          :error  error
+          :msg    (when msg-type msg)
+          :timbre/vargs vargs})))})

commit 6030c472aecb0f47526f8f3779c2b9594587fb59

[fix] Timbre->Telemere appender: missing error regression
Recent 47af80319dd6a6922ad9617cbebc3dc632fdb689 (in Telemere v1.2.0)
accidentally removed errors from Timbre->Telemere signals.

main/src/taoensso/telemere/timbre.cljc
@@ -190,11 +190,11 @@
    :min-level nil
    :fn
    (fn [data]
-     (let [{:keys [instant level context msg-type vargs
+     (let [{:keys [instant level context ?err msg-type vargs
                    ?ns-str ?file ?line ?column]} data
 
            format-msg? (enc/identical-kw? msg-type :f)
-           [error msg vargs] (parse-vargs format-msg? vargs)]
+           [_error msg vargs] (parse-vargs format-msg? vargs)]
 
        (taoensso.telemere/signal!
          {:kind :timbre
@@ -206,7 +206,7 @@
           :coords (when ?line [?line ?column])
           :file   ?file ; Non-standard, goes to kvs
 
-          :error  error
+          :error  ?err
           :msg    (when msg-type msg)
 
           :timbre/vargs vargs})))})